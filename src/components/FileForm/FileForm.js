945e752aa328838272baf020ca8db533faeafc37
Glenn Chen <glenn.jun.chen@gmail.com>
2022-04-16 14:24:59 -0700

    skeleton submit button

--- src/components/FileForm/FileForm.js
+++ src/components/FileForm/FileForm.js
@@ -4,33 +4,39 @@ import "./FileForm.css";
 export default class FileForm extends Component {
 
 	constructor(props) {
-    super(props);
-    this.state = {value: ''};
+		super(props);
+		this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
-  }
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
 	handleChange(event) {
 		this.setState({value: event.target.value});
 	}
 
-  render() {
-  	return (
-      <div class="fileFormContainer">
+	handleSubmit(event) {
+		event.preventDefault();
+	}
 
-        <label for="avatar" class="uploadCaption">Upload a photo of your ingredients</label>
-				<label class="custom-file-upload">
+	render() {
+		return (
+			<div class="fileFormContainer">
+				<form>
+					<label for="avatar" class="uploadCaption">Upload a photo of your ingredients</label>
+					<label class="custom-file-upload">
 					<input 
 						type="file"
-						id="photo" 
+						id="photo"
 						name="photo"
 						accept="image/png, image/jpeg"
 						onChange={this.handleChange}
 					/>
 						Choose File
-				</label>
-    	</div>
-    );
-  }
+					</label>
+					<button class="submitButton" onClick={this.handleSubmit}>Find Recipes</button>
+				</form>
+			</div>
+		);
+	}
 }
