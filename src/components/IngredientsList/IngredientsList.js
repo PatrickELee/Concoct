6c3a50af9e6345bb3c3bc4c144a352c4d94c74b9
Glenn Chen <glenn.jun.chen@gmail.com>
2022-04-16 17:47:16 -0700

    used mapping for lists

--- src/components/IngredientsList/IngredientsList.js
+++ src/components/IngredientsList/IngredientsList.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import "./IngredientsList.css";
 import "@fontsource/inter";
 
+let results = ["Tomatoes", "Spaghetti noodles", "Water", ""];
+
 export default class FileForm extends Component {
 
     render() {
+        let indices = [];
+        for (let i = 0; i < results.length; i++)
+            indices.push(i);
+        
         return (
             <div class="ingredientsContainer">
                 <h2>Ingredients Recognized</h2>
                 <div class="grid-container">
-                    <p>food item</p>
-                    <p>food item</p>
-                    <p>food item</p>
-                    <p>food item</p>
+                    {indices.map(index => (<p>{results[index]}</p>))}
                 </div>
             </div>
         );
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import "./Recipe.css";
+import Spaghetti from "../../spaghetti.jpeg";
+
+let results = ["Tomatoes", "Spaghetti noodles", "Water"];
 
 export default class Recipe extends Component {
 
     render() {
+        let indices = [];
+        for (let i = 0; i < results.length; i++)
+            indices.push(i);
+
         return (
             <div class="recipeContainer">
                 <h3>{this.props.name}</h3>
+                <img src={Spaghetti} />
+                <h4>Ingredients used</h4>
                 <ul>
-                    <li>Tomato sauce</li>
-                    <li>Spaghetti noodles</li>
-                    <li>FEFIJAOFIJOAIEJFAWWWWIEFOJFAOEIFJPAO</li>
+                    {indices.map(index => (<li>{results[index]}</li>))}
                 </ul>
             </div>
         );
